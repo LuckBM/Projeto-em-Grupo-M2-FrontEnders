282072ccdb6f30bbc65a35c43dd493c3bcd0299c
Th <116724740+ThiagoAbreuBR@users.noreply.github.com>
2022-12-17 18:03:35 -0300

    Update planos.js
    JS da página de planos

--- js/planos.js
+++ js/planos.js
@@ -1,87 +1,80 @@
 // Evento de click no SAIBA MAIS no PLANO BÁSICO
 document.getElementById("basicMais").addEventListener('click', basic);
 function basic() {
-    document.getElementById('sobreBasic').innerHTML = 'MINHA NOSSA SENHORA O IMPOSSIVEL ACONTECEU';
+    document.getElementById('sobreBasic').innerHTML = 'Assistente de voz';
+    document.getElementById('sobreBasicII').innerHTML = 'Correção de códigos em JS';
+    document.getElementById('sobreBasicIII').innerHTML = 'Audiobook de JS';
 }
 
 // Evento de exclusão de texto PLANO BÁSICO ao clicar em qualquer um dos planos
-document.getElementById("medium").addEventListener('click', basicExit);
-function basicExit() {
-    document.getElementById('sobreBasic').innerHTML = '';
-}
-
 document.getElementById("ultra").addEventListener('click', basicExit2);
 function basicExit2() {
     document.getElementById('sobreBasic').innerHTML = '';
+    document.getElementById('sobreBasicII').innerHTML = '';
+    document.getElementById('sobreBasicIII').innerHTML = '';
 }
 
 document.getElementById("ultimate").addEventListener('click', basicExit3);
 function basicExit3() {
     document.getElementById('sobreBasic').innerHTML = '';
-}
-
-// Evento de click no SAIBA MAIS no PLANO MÉDIO
-document.getElementById("mediumMais").addEventListener('click', medium);
-function medium() {
-    document.getElementById('sobreMedium').innerHTML = 'MINHA NOSSA SENHORA O IMPOSSIVEL ACONTECEU';
-}
-
-// Evento de exclusão de texto PLANO MÉDIO ao clicar em qualquer um dos planos
-document.getElementById("basic").addEventListener('click', mediumExit);
-function mediumExit() {
-    document.getElementById('sobreMedium').innerHTML = '';
-}
-
-document.getElementById("ultra").addEventListener('click', mediumExit2);
-function mediumExit2() {
-    document.getElementById('sobreMedium').innerHTML = '';
-}
-
-document.getElementById("ultimate").addEventListener('click', mediumExit3);
-function mediumExit3() {
-    document.getElementById('sobreMedium').innerHTML = '';
+    document.getElementById('sobreBasicII').innerHTML = '';
+    document.getElementById('sobreBasicII').innerHTML = '';
 }
 
 // Evento de click no SAIBA MAIS no PLANO ULTRA
 document.getElementById("ultraMais").addEventListener('click', ultra);
 function ultra() {
-    document.getElementById('sobreUltra').innerHTML = 'MINHA NOSSA SENHORA O IMPOSSIVEL ACONTECEU';
+    document.getElementById('sobreUltra').innerHTML = 'Todos benefícios do plano BÁSICO';
+    document.getElementById('sobreUltraII').innerHTML = 'Assistente de rotina';
+    document.getElementById('sobreUltraIII').innerHTML = 'Correção de códigos em PHP';
+    document.getElementById('sobreUltraIV').innerHTML = 'Audiobooks de PHP';
 }
 
 // Evento de exclusão de texto PLANO ULTRA ao clicar em qualquer um dos planos
 document.getElementById("basic").addEventListener('click', ultraExit);
 function ultraExit() {
     document.getElementById('sobreUltra').innerHTML = '';
-}
-
-document.getElementById("medium").addEventListener('click', ultraExit2);
-function ultraExit2() {
-    document.getElementById('sobreUltra').innerHTML = '';
+    document.getElementById('sobreUltraII').innerHTML = '';
+    document.getElementById('sobreUltraIII').innerHTML = '';
+    document.getElementById('sobreUltraIV').innerHTML = '';
 }
 
 document.getElementById("ultimate").addEventListener('click', ultraExit3);
 function ultraExit3() {
     document.getElementById('sobreUltra').innerHTML = '';
+    document.getElementById('sobreUltraII').innerHTML = '';
+    document.getElementById('sobreUltraIII').innerHTML = '';
+    document.getElementById('sobreUltraIV').innerHTML = '';
 }
 
 // Evento de click no SAIBA MAIS no PLANO ULTIMATE
 document.getElementById("ultimateMais").addEventListener('click', ultimate);
 function ultimate() {
-    document.getElementById('sobreUltimate').innerHTML = 'MINHA NOSSA SENHORA O IMPOSSIVEL ACONTECEU';
+    document.getElementById('sobreUltimate').innerHTML = 'Todos benefícios dos planos BÁSICO e ULTRA';
+    document.getElementById('sobreUltimateII').innerHTML = 'Correção de códigos em Phython';
+    document.getElementById('sobreUltimateIII').innerHTML = 'Audiobook de Phython';
+    document.getElementById('sobreUltimateIV').innerHTML = 'Persolização de voz da IA';
+    document.getElementById('sobreUltimateV').innerHTML = 'Correção de códigos em Phython';
 }
 
 // Evento de exclusão de texto PLANO ULTIMATE ao clicar em qualquer um dos planos
 document.getElementById("basic").addEventListener('click', ultimateExit);
 function ultimateExit() {
     document.getElementById('sobreUltimate').innerHTML = '';
-}
-
-document.getElementById("medium").addEventListener('click', ultimateExit2);
-function ultimateExit2() {
     document.getElementById('sobreUltimate').innerHTML = '';
+    document.getElementById('sobreUltimateII').innerHTML = '';
+    document.getElementById('sobreUltimateIII').innerHTML = '';
+    document.getElementById('sobreUltimateIV').innerHTML = '';
+    document.getElementById('sobreUltimateV').innerHTML = '';
 }
 
 document.getElementById("ultra").addEventListener('click', ultimateExit3);
 function ultimateExit3() {
     document.getElementById('sobreUltimate').innerHTML = '';
+    document.getElementById('sobreUltimate').innerHTML = '';
+    document.getElementById('sobreUltimate').innerHTML = '';
+    document.getElementById('sobreUltimateII').innerHTML = '';
+    document.getElementById('sobreUltimateIII').innerHTML = '';
+    document.getElementById('sobreUltimateIV').innerHTML = '';
+    document.getElementById('sobreUltimateV').innerHTML = '';
 }
